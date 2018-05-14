c51b467c1b54508ce4d584934d131aedc0ccc2d1
Ryan Harrington <Ryanaharrington5@gmail.com>
2018-05-14 08:59:08 -0600

    Add length of favorites to view favorites btn

--- src/stateless-components/footer/index.js
+++ src/stateless-components/footer/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Navigation from '../navigation/index';
 
-const Footer = ({ updateCards, displayFavorites }) => {
+const Footer = ({ updateCards, displayFavorites, favorites }) => {
   return (
     <div>
       <Navigation 
         updateCards={updateCards}
         displayFavorites={displayFavorites}
+        favorites={favorites}
       />
       <img className="dashboard-pic" src="http://blog.ocad.ca/wordpress/digf6l01-fw201402-01/files/2014/10/tumblr_larntzqOpV1qz5zvo.png" />
     </div>
@@ -16,7 +17,8 @@ const Footer = ({ updateCards, displayFavorites }) => {
 
 Footer.propTypes = {
   updateCards: PropTypes.func.isRequired,
-  displayFavorites: PropTypes.func.isRequired
+  displayFavorites: PropTypes.func.isRequired,
+  favorites: PropTypes.array
 };
 
 export default Footer;
